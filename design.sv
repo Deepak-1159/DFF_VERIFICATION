4bd80767b1842cd3d5435e71a3fc911197ef7b9a
Deepak-1159 <149907613+Deepak-1159@users.noreply.github.com>
2024-03-26 18:52:48 -0700

    Update design.sv

--- design.sv
+++ design.sv
@@ -1,25 +1,26 @@
-module Pro1(dff dif);
-
-always@(posedge dif.clk)
-	begin
-	if(dif.rst)
-		dif.dout<='0;
-	else
-		dif.dout<=dif.din;
-	end
-
+// Define a module named "dff" with an interface "dff_if"
+ 
+module dff (dff_if vif);
+ 
+  // Always block triggered on the positive edge of the clock signal
+  always @(posedge vif.clk)
+    begin
+      // Check if the reset signal is asserted
+      if (vif.rst == 1'b1)
+        // If reset is active, set the output to 0
+        vif.dout <= 1'b0;
+      else
+        // If reset is not active, pass the input value to the output
+        vif.dout <= vif.din;
+    end
+  
 endmodule
-
-interface dff;
-
-logic clk;
-logic rst;
-logic din;
-logic dout;
-
-
-
-
+ 
+// Define an interface "dff_if" with the following signals
+interface dff_if;
+  logic clk;   // Clock signal
+  logic rst;   // Reset signal
+  logic din;   // Data input
+  logic dout;  // Data output
+  
 endinterface
-
-
